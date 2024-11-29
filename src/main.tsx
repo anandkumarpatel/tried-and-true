ba3ddf18ee793c218feb3396893176e52e64dbde
Anand Patel <anandkumarpatel@gmail.com>
2024-11-28 20:49:17 -0800

    hash routes

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,11 @@
-import { RouterProvider, createRouter } from '@tanstack/react-router'
+import { RouterProvider, createRouter, createHashHistory } from '@tanstack/react-router'
 import { StrictMode } from 'react'
 import ReactDOM from 'react-dom/client'
 import { routeTree } from './routeTree.gen'
 
+const hashHistory = createHashHistory()
 // Create a new router instance
-const router = createRouter({ routeTree })
+const router = createRouter({ routeTree, history: hashHistory })
 
 // Register the router instance for type safety
 declare module '@tanstack/react-router' {
