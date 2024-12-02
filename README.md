6f98b05213d1cfe4ba92c015423f464772e11c38
Anand Patel <anandkumarpatel@gmail.com>
2024-12-01 19:42:53 -0800

    add web worker

--- README.md
+++ README.md
@@ -2,11 +2,14 @@
 
 # TODO
 
-- delete recipe
-- revert recipe
+- edit & revert recipe
 - add proportion calculator
 - add tags
 - add rating
 - add search
 - select recipe and generate shopping list
 - create cookbook
+- multi user
+- calorie counter
+- photo upload
+- substitution suggestions
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,54 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
+import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
-  plugins: [TanStackRouterVite(), react()],
+  plugins: [
+    TanStackRouterVite(),
+    react(),
+    VitePWA({
+      injectRegister: 'auto',
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.ico', 'apple-touch-icon-180x180', 'maskable-icon-512x512.png'],
+      manifest: {
+        name: 'Tried & True',
+        short_name: 'Tried&True',
+        description: 'Recipe collector',
+        theme_color: '#ffffff',
+        share_target: {
+          action: '#/share-target',
+          method: 'GET',
+          params: {
+            title: 'title',
+            text: 'text',
+            url: 'url',
+          },
+        },
+        icons: [
+          {
+            src: 'pwa-64x64.png',
+            sizes: '64x64',
+            type: 'image/png',
+          },
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+          {
+            src: 'maskable-icon-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'maskable',
+          },
+        ],
+      },
+    }),
+  ],
 })
