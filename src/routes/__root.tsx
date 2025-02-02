13d4b58a5e57ef51932d6f4a3bf539dc0e235971
Anand Patel <anandkumarpatel@gmail.com>
2025-02-02 15:45:36 -0800

    disable tan on dev

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -11,7 +11,7 @@ export const Route = createRootRoute({
         </Link>{' '}
       </div>
       <Outlet />
-      <TanStackRouterDevtools />
+      {window.location.hostname === 'localhost' && <TanStackRouterDevtools />}
     </>
   ),
 })
